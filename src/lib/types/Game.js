ca638336e332d07a7aadcd11275973270927a53d
Megakuul <linus.moser@megakuul.ch>
2025-02-25 23:45:29 +0100

    feat: add some example stuff

---
+++ src/lib/types/Game.js
@@ -0,0 +1,20 @@
+/**
+ * @typedef {Object} Game
+ * @property {string} version
+ * @property {string} seed
+ * @property {Player[]} players
+ */
+
+/**
+ * @typedef {Object} Player
+ * @property {string} name
+ */
+
+export function alarm() {
+    generate()
+    console.log("asdf")
+}
+
+function generate() {
+    console.log("i generated")
+}
---
+++ src/routes/game/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { alarm } from "$lib/types/Game.js";
+  let Game = $state();
+
+  alarm()
+</script>
+
+<div>
+  asdfasdf
+</div>
+
+<style>
+
+</style>

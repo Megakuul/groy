db7510a8a5c1c03a0da1f7238f7f84e6d7dfa715
Megakuul <linus.moser@megakuul.ch>
2025-02-27 23:43:52 +0100

    chore: add poc game structure

--- src/lib/types/Game.js
+++ src/lib/types/Game.js
@@ -1,8 +1,13 @@
 /**
  * @typedef {Object} Game
  * @property {string} version
- * @property {string} seed
- * @property {Player[]} players
+ * @property {number} revision
+ * @property {number} columns
+ * @property {number} rows
+ * @property {Object.<string, number>} indexes
+ * @property {Block[]} blocks
+ * 
+ * @property {Object.<string, Player>} players
  */
 
 /**
@@ -10,11 +15,46 @@
  * @property {string} name
  */
 
-export function alarm() {
-    generate()
-    console.log("asdf")
-}
+/**
+ * @typedef {Object} Block
+ * @property {"static" | "dynamic"} type
+ * @property {StaticBlock} static
+ * @property {DynamicBlock} dynamic
+ */
 
-function generate() {
-    console.log("i generated")
-}
+/**
+ * @typedef {Object} StaticBlock
+ * @property {"crystal" | "wood" | "iron" | "uranium"} type
+ */
+
+/**
+ * @typedef {Object} DynamicBlock
+ * @property {number} hitpoints
+ * @property {string} owner
+ * @property {number} level
+ * @property {"town-hall" | "crystal-mine" | "iron-mine" | "cannon"} type
+ * @property {TownHall} town_hall
+ * @property {CrystalMine} crystal_mine
+ * @property {IronMine} iron_mine
+ * @property {Cannon} cannon
+ */
+
+/**
+ * @typedef {Object} TownHall
+ * @property {number} crystals
+ */
+
+/**
+ * @typedef {Object} CrystalMine
+ * @property {number} production
+ */
+
+/**
+ * @typedef {Object} IronMine
+ * @property {number} production
+ */
+
+/**
+ * @typedef {Object} Cannon
+ * @property {number} bullets
+ */
